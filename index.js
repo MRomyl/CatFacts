4309dfc0824b01c8e97853d8571bb0c98db3b35c
MRomyl <romyl.martushev@gmail.com>
2026-05-09 20:01:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://meowfacts.herokuapp.com/";
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        const fact = response.data.data[0];
+        res.render("index.ejs", { data: fact });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on localhost:${port}`);
+});
